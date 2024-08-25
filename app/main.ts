29414d99878804b0468499bbc70f4b346b69d5a8
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-25 17:51:17 +0300

    refactor: modularize server setup with router and handlers
    - Moved individual request handlers to separate modules
    - Simplified the server logic by delegating request processing to the router

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,6 @@
 import net from "net";
-import path from "path";
-import url from "url";
-import fs from "fs/promises";
-import zlib from "zlib";
 
 import { Request } from "./types";
-
-import createResponse from "./utils/createHttpResponse";
 
 import {
   parseRequestBody,
@@ -14,7 +8,14 @@ import {
   parseRequestLine,
 } from "./utils/parsers";
 
-console.log("------------");
+import Router from "./router";
+import "./routes/index";
+
+import handleNotFound from "./handlers/notFoundHandler";
+
+console.log("------------ Project start ------------");
+
+const router = Router.getInstance();
 
 const server = net.createServer((socket) => {
   console.log("Connected");
@@ -23,37 +24,18 @@ const server = net.createServer((socket) => {
     try {
       const rawData = data.toString("ascii");
 
-      const { method, pathname } = parseRequestLine(rawData);
-      const headers = parseRequestHeaders(rawData);
+      const requestLine = parseRequestLine(rawData);
+      const headers = parseRequestHeaders(rawData, requestLine.extension);
       const body = parseRequestBody(rawData, headers);
 
       const request: Request = {
-        rawData: rawData,
-        method,
-        pathname,
+        ...requestLine,
         ...headers,
         ...body,
+        params: {},
       };
 
-      if (method === "GET") {
-        if (pathname === "/" || pathname === "/index.html") {
-          handleOKResponse(socket, request);
-        } else if (pathname.startsWith("/echo")) {
-          handleTextResponse(socket, request);
-        } else if (pathname === "/user-agent") {
-          handleUserAgent(socket, request);
-        } else if (pathname.startsWith("/files")) {
-          handleGetFileContent(socket, request);
-        } else {
-          handleNotFound(socket);
-        }
-      } else {
-        if (pathname.startsWith("/files")) {
-          handleCreateFileWithContent(socket, request);
-        } else {
-          handleNotFound(socket);
-        }
-      }
+      router.handleRequest(request, socket);
     } catch (error) {
       handleNotFound(socket);
     }
@@ -65,126 +47,3 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost");
-
-// - GET Handlers
-function handleOKResponse(socket: net.Socket, req: Request) {
-  const { pathname, host } = req;
-
-  const parsedUrl = url.format({
-    protocol: "http",
-    pathname,
-    host,
-  });
-
-  const response = createResponse(200, {
-    contentType: "text/plain",
-    contentLength: parsedUrl.length,
-    responseData: parsedUrl,
-  });
-
-  socket.write(response);
-  socket.end(() => {
-    console.log("Response is send: ", response.toString());
-    console.log("Request is valid, URL Path -> ", parsedUrl);
-  });
-}
-
-function handleTextResponse(socket: net.Socket, req: Request) {
-  const { pathname } = req;
-
-  const pathnameParts = pathname.split("/");
-  const lastPartPathname = pathnameParts[pathnameParts.length - 1];
-
-  zlib.gzip(lastPartPathname, (err, compressedData) => {
-    if (err) {
-      console.error("Error compressing data:", err);
-      handleNotFound(socket);
-      return;
-    }
-
-    const response = createResponse(200, {
-      contentType: "application/octet-stream",
-      contentLength: compressedData.byteLength,
-      responseData: compressedData.toString("binary"),
-      contentEncoding: "gzip",
-    });
-
-    socket.write(response);
-    socket.end(() => {
-      console.log("Response sent with compressed data:", compressedData);
-    });
-  });
-}
-
-function handleUserAgent(socket: net.Socket, req: Request) {
-  try {
-    const { userAgent } = req;
-
-    const response = createResponse(200, {
-      contentType: "text/plain",
-      contentLength: userAgent.length,
-      responseData: userAgent,
-    });
-
-    socket.write(response);
-    socket.end(() => {
-      console.log("Response is send: ", response.toString());
-    });
-  } catch (error) {
-    socket.end("HTTP/1.1 404 Not Found\r\n\r\n");
-    return;
-  }
-}
-
-async function handleGetFileContent(socket: net.Socket, req: Request) {
-  const { pathname } = req;
-  const lastPartOfPathname = pathname.split("/files")[1];
-
-  const dirPath = path.join(__dirname, "../", "/public");
-  const files = await fs.readdir(dirPath);
-
-  if (!files.some((f) => `/${f}` === lastPartOfPathname)) {
-    socket.end(createResponse(404));
-    return;
-  }
-
-  const filePath = path.join(__dirname, "../", "/public", lastPartOfPathname);
-  const fileInBytes = await fs.readFile(filePath);
-
-  const response = createResponse(200, {
-    contentType: "application/octet-stream",
-    contentLength: fileInBytes.byteLength,
-    responseData: fileInBytes.toString("binary"),
-  });
-
-  socket.write(response);
-  socket.end(() => {
-    console.log("Response is send: ", response.toString());
-  });
-}
-
-// - POST Handlers
-async function handleCreateFileWithContent(socket: net.Socket, req: Request) {
-  const { body, pathname } = req;
-
-  const lastPartOfPathname = pathname.split("/files")[1];
-  const filePath = path.join(__dirname, "../", "/public", lastPartOfPathname);
-
-  await fs.writeFile(filePath, body, { flag: "w+" });
-
-  const response = createResponse(201);
-
-  socket.write(response);
-  socket.end(() => {
-    console.log("Response is send: ", response.toString());
-  });
-}
-
-// - Error Handlers
-function handleNotFound(socket: net.Socket) {
-  const response = Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n");
-  socket.write(response);
-  socket.end(() => {
-    console.log("Request is invalid - resource not found");
-  });
-}
