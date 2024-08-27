e733a09a5de6412b40eaa5ab10435625415a7eec
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-27 20:38:55 +0300

    feat: Implement RequestParser class for parsing HTTP requests
    - Added RequestParser class to handle the parsing of raw HTTP request data.
    - The class extracts the request line, headers, and body from the raw request string.
    - Includes methods for parsing and retrieving the request line (`getRequestLine`), headers (`getRequestHeaders`), and body (`getRequestBody`).
    - Use regex for extracting extensions and normalizing paths for accurate content type handling.

---
+++ app/router-engine/Parser.ts
@@ -0,0 +1,74 @@
+import {
+  RequestBody,
+  RequestHeadersOptions,
+  RequestLineOptions,
+} from "../types";
+import { mimeType } from "../utils/mimeTypes";
+
+export class RequestParser {
+  private rawRequest: string;
+
+  private requestLine: string;
+  private headersString: string[];
+  private body: string;
+
+  constructor(data: Buffer) {
+    this.rawRequest = data.toString("ascii");
+
+    const [head, body] = this.rawRequest.split("\r\n\r\n");
+    const [requestLine, ...ls] = head.split("\r\n");
+
+    this.requestLine = requestLine;
+    this.headersString = ls;
+    this.body = body;
+  }
+
+  public getRequestLine(): RequestLineOptions {
+    const [method, pathname] = this.requestLine.split(" ");
+
+    const regexForExtension = new RegExp(/(?<ext>\.[\w]+)$/);
+    let group = pathname.match(regexForExtension)?.groups;
+
+    let extension = group?.ext;
+    let isCompressed = false;
+
+    // TODO - Refactor this because it's look awful
+    if (extension && extension === ".gz") {
+      const originalPath = pathname.substring(0, pathname.length - 1 - 2);
+      extension = originalPath.match(regexForExtension)?.groups?.ext;
+      isCompressed = true;
+    }
+
+    if (!mimeType[extension as keyof typeof mimeType]) {
+      extension = undefined;
+    }
+
+    return {
+      method,
+      pathname,
+      extension,
+    };
+  }
+
+  public getRequestHeaders(): RequestHeadersOptions {
+    const headers = this.headersString
+      .map((e) => {
+        return e.split(": ");
+      })
+      .reduce(
+        (hs, [key, value]) =>
+          Object.defineProperty(hs, key.toLowerCase(), {
+            enumerable: true,
+            value,
+            writable: false,
+          }),
+        {}
+      ) as RequestHeadersOptions;
+
+    return headers;
+  }
+
+  public getRequestBody() {
+    return this.body;
+  }
+}
