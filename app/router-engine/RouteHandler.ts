23162df858fb6172c85335741db6ca1ae862f119
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-27 20:29:06 +0300

    feat: Implement RouteHandler class for managing and matching routes
    - Added RouteHandler class that allows adding and finding routes based on HTTP methods and paths.
    - Supports dynamic route parameters, such as `/user/:id`, by parsing the path and extracting parameter names.
    - Handles both GET and POST requests.
    - The `parsePath` method normalizes paths and extracts dynamic segment names.
    - The `matchPath` method matches incoming request paths against stored routes using regex.
    - The `getParams` method extracts values for dynamic segments from matched paths and associates them with parameter names.

---
+++ app/router-engine/RouteHandler.ts
@@ -0,0 +1,51 @@
+import { HttpHandler } from "../types";
+
+export class RouteHandler {
+  private routes: {
+    [key: string]: {
+      [method: string]: { paramNames: string[]; handler: HttpHandler };
+    };
+  } = {};
+
+  public addRoute(method: "GET" | "POST", path: string, handler: HttpHandler) {
+    const { normalizedPath, paramNames } = parsePath(path);
+    if (!this.routes[normalizedPath]) {
+      this.routes[normalizedPath] = {};
+    }
+    this.routes[normalizedPath][method] = { handler, paramNames };
+  }
+
+  public findRoute(method: string, pathname: string) {
+    for (const normalizedPath in this.routes) {
+      const { handler, paramNames } = this.routes[normalizedPath][method] || {};
+      const match = matchPath(normalizedPath, pathname);
+      if (match) {
+        const params = getParams(match, paramNames);
+        return { handler, params };
+      }
+    }
+    return null;
+  }
+}
+
+function parsePath(path: string) {
+  const paramNames: string[] = [];
+  const normalizedPath = path.replace(/:(\w+)/g, (_, paramName) => {
+    paramNames.push(paramName);
+    return "([^/]+)";
+  });
+  return { normalizedPath, paramNames };
+}
+
+function matchPath(normalizedPath: string, pathname: string) {
+  const regex = new RegExp(`^${normalizedPath}$`);
+  return pathname.match(regex);
+}
+
+function getParams(match: RegExpMatchArray, paramNames: string[]) {
+  const params: { [key: string]: string } = {};
+  paramNames.forEach((paramName, index) => {
+    params[paramName] = match[index + 1];
+  });
+  return params;
+}
