7cc2991c60917f1e9c4cc9430354776e37145e5f
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-29 19:22:20 +0300

    feat: Implement Database class with CSV-based table management
    - Added `Database` class with methods to create, load, list, and delete tables.
    - Tables are stored as CSV files with header rows representing columns.
    - Load table references without loading the full content into memory.
    - Added support for dynamic column types and initial file validation.

---
+++ app/database/Database.ts
@@ -0,0 +1,84 @@
+import fsSync from "fs";
+import fs from "fs/promises";
+import path from "path";
+import Table from "./_Table";
+import { createArrFromCSVLine, createCSVRow } from "./_utils";
+
+export class Database {
+  private databaseName: string;
+  private dbDirPath: string;
+  private tables: { [key: string]: Table<string[]> } = {};
+
+  constructor(databaseName: string, dbDirPath: string) {
+    this.databaseName = databaseName;
+    this.dbDirPath = dbDirPath;
+    this.load();
+  }
+
+  public async createOrGetTable<ColumnTypes extends readonly string[]>(
+    tableName: string,
+    columns: [...ColumnTypes]
+  ): Promise<Table<typeof columns>> {
+    const tableFilePath = path.join(this.dbDirPath, `${tableName}.csv`);
+    const header = createCSVRow(columns);
+
+    if (this.tables[tableName]) {
+      return this.tables[tableName] as Table<typeof columns>;
+    }
+
+    await fs.writeFile(tableFilePath, header);
+    const table = new Table(tableName, tableFilePath, columns);
+    this.tables[tableName] = table;
+    return table;
+  }
+
+  public async deleteTable(name: string) {
+    const table = this.findTable(name);
+
+    if (!table) {
+      throw new Error("Table does'not exist.");
+    }
+    delete this.tables[table.tableName];
+    await fs.rmdir(table.tablePath);
+  }
+
+  public listTables(): string[] {
+    return Object.keys(this.tables);
+  }
+
+  private findTable(name: string) {
+    return this.tables[name];
+  }
+
+  private load() {
+    try {
+      fsSync.accessSync(this.dbDirPath);
+    } catch {
+      fsSync.mkdirSync(this.dbDirPath);
+    } finally {
+      const files = fsSync.readdirSync(this.dbDirPath);
+      for (const file of files) {
+        const tableName = path.basename(file, ".csv");
+        const tablePath = path.join(this.dbDirPath, file);
+        const fd = fsSync.openSync(tablePath, "r");
+
+        try {
+          const buffer = Buffer.alloc(512);
+          let bytesRead = fsSync.readSync(fd, buffer, 0, buffer.length, 0);
+          let data = buffer.subarray(0, bytesRead).toString();
+
+          const newLineIndex = data.indexOf("\n");
+
+          if (newLineIndex !== -1) {
+            const rawColumns = data.substring(0, newLineIndex);
+            const columns = createArrFromCSVLine(rawColumns);
+            const table = new Table(tableName, tablePath, columns);
+            this.tables[tableName] = table;
+          }
+        } finally {
+          fsSync.closeSync(fd);
+        }
+      }
+    }
+  }
+}
