2ddd6f197ca3580e47d5e69cefc0f073ca45e040
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-30 16:04:52 +0300

    refactor: `handleNotFound` to serve a custom 404 page and improve response handling

--- app/handlers/notFoundHandler.ts
+++ app/handlers/notFoundHandler.ts
@@ -1,9 +1,25 @@
-import { Socket } from "../types";
+import path from "path";
+import fs from "fs/promises";
+import { Response } from "../types";
 
-export default function handleNotFound(socket: Socket) {
-  const response = Buffer.from("HTTP/1.1 404 Not Found\r\n\r\n");
-  socket.write(response);
-  socket.end(() => {
-    console.log("Request is invalid - resource not found");
-  });
+const STATIC_PATH_DIR = path.join(process.cwd(), "public");
+
+export default async function handleNotFound(res: Response) {
+  const pagePath = path.join(STATIC_PATH_DIR, "pages", "404.html");
+
+  try {
+    await fs.access(pagePath);
+  } catch {
+    return res.status(404).end();
+  }
+  const file = await fs.readFile(pagePath);
+
+  res
+    .status(404)
+    .set({
+      "Content-Type": "text/html",
+      "Content-Length": file.byteLength,
+    })
+    .end(file);
+  return;
 }
