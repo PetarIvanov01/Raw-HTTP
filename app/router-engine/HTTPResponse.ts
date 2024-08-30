19b6d55620c4d955d92c0a6b2700693696f49621
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-30 15:35:32 +0300

    feat(server): Create HTTPResponse class for handling HTTP responses
    - 'set': Sets the headers, either individually or in bulk. Function overload is used.
    - 'status': Sets the status code of the response.
    - 'writeHead': Sends the headers before the body, ensuring they are only sent once.
    - 'end':  Send response and close the connection.
    - 'send': Combined header and data sending in a single call.

---
+++ app/router-engine/HTTPResponse.ts
@@ -0,0 +1,69 @@
+import { Socket } from "../types";
+import createResponse from "../utils/createHttpResponse";
+
+type Headers = Record<string, string | number>;
+type KeyOfHeaders = keyof Headers;
+export default class HTTPResponse {
+  private socket: Socket;
+  private headersSent: boolean = false;
+  private statusCode: number = 200;
+  private headers: Headers = {};
+
+  constructor(socket: Socket) {
+    this.socket = socket;
+  }
+
+  public set(headers: Headers): this;
+  public set(key: KeyOfHeaders, value: string): this;
+
+  public set(keyOrHeaders: Headers | KeyOfHeaders, value?: string): this {
+    if (typeof keyOrHeaders === "string") {
+      const key = keyOrHeaders.toLowerCase();
+      if (value) {
+        this.headers[key] = value;
+      }
+    } else {
+      for (const [key, val] of Object.entries(keyOrHeaders)) {
+        this.headers[key.toLowerCase()] = val;
+      }
+    }
+
+    return this;
+  }
+
+  public status(code: number) {
+    this.statusCode = code;
+    return this;
+  }
+
+  private writeHead() {
+    if (!this.headersSent) {
+      const responseHeaders = createResponse({
+        statusCode: this.statusCode,
+        headers: this.headers,
+      });
+
+      this.socket.write(responseHeaders);
+      this.headersSent = true;
+    }
+  }
+
+  public write(data: Buffer | string) {
+    this.writeHead();
+    this.socket.write(data);
+  }
+
+  public end(data?: Buffer | string) {
+    if (data) {
+      this.write(data);
+    }
+    this.socket.end(() => {
+      console.log("Response is sent and socket is closed");
+    });
+  }
+
+  public send(data: Buffer | string) {
+    this.writeHead();
+    this.socket.end(data);
+  }
+}
