2f845e1fa9c7edd89bf46230b2fab6ebfd37ba6f
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-30 15:49:48 +0300

    feat: the function read a file in chunks starting from a specified offset and process each line within those chunks.
    - Handles file reading from a specific offset.
    - Uses a callback function to process each line, with support for early termination.

---
+++ app/utils/readFileInChunks.ts
@@ -0,0 +1,65 @@
+import * as fs from "fs/promises";
+
+export default function processFileInChunks(startOffset = 0) {
+  return async (
+    filePath: string,
+    chunkSize: number,
+    callback: (line: string, start: number, end: number) => boolean
+  ): Promise<void> => {
+    const fd = await fs.open(filePath, "r");
+
+    // This offest should be at the end of the table header
+    let offset = startOffset;
+    let remainder = "";
+    let currentByte = offset;
+    // Debugging
+    let iterations = 0;
+
+    try {
+      const buffer = Buffer.alloc(chunkSize);
+
+      while (true) {
+        iterations++;
+
+        const { bytesRead } = await fd.read(buffer, 0, chunkSize, offset);
+
+        if (bytesRead === 0) break;
+
+        const chunk =
+          remainder + buffer.subarray(0, bytesRead).toString("utf-8");
+
+        const lastNewlineIndex = chunk.lastIndexOf("\n");
+
+        if (lastNewlineIndex !== -1) {
+          const lines = chunk.slice(0, lastNewlineIndex).split("\n");
+          remainder = chunk.slice(lastNewlineIndex + 1);
+
+          for (const line of lines) {
+            const lineLength = Buffer.byteLength(line, "utf-8") + 1; // +1 for newline
+
+            const lineStart = currentByte;
+            const lineEnd = currentByte + lineLength;
+
+            const done = callback(line, lineStart, lineEnd);
+            if (done) {
+              return;
+            }
+            currentByte = lineEnd;
+          }
+        } else {
+          remainder = chunk;
+        }
+
+        offset += bytesRead;
+      }
+
+      if (remainder) {
+        const lineStart = currentByte;
+        const lineEnd = currentByte + Buffer.byteLength(remainder, "utf-8");
+        callback(remainder, lineStart, lineEnd);
+      }
+    } finally {
+      await fd.close();
+    }
+  };
+}
