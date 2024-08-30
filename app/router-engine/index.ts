59586af49031c45d89b28ff19ba148ef3a242cac
PetarIvanov01 <123456789petar45@gmail.com>
2024-08-30 15:44:06 +0300

    chore: Imported and exported `Router` as the default module to streamline access to the main routing class.

---
+++ app/router-engine/index.ts
@@ -0,0 +1,9 @@
+import { Router } from "./Router";
+
+export { RouteHandler } from "./RouteHandler";
+export { MiddlewareManager } from "./MiddlewareManager";
+export { RequestProcessor } from "./RequestProcessor";
+export { RequestParser } from "./RequestParser";
+export { HTTPResponse } from "./HTTPResponse";
+
+export default Router;
