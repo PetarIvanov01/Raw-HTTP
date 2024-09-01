c7dc7cbf1fcf1a79ccc7e1114d181a1490d2a064
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-01 13:26:53 +0300

    feat: Implement custom file system abstraction
    - Created `customFileSystem` module to provide a unified interface for synchronous and asynchronous file system operations.

---
+++ app/lib/fileSystem.ts
@@ -0,0 +1,37 @@
+import * as fs from "node:fs";
+import * as fsPromises from "node:fs/promises";
+import * as path from "path";
+
+export const customFileSystem = {
+  accessSync: fs.accessSync,
+  writeFileSync: fs.writeFileSync,
+  appendFileSync: fs.appendFileSync,
+  statSync: fs.statSync,
+  readFileSync: fs.readFileSync,
+  existsSync: fs.existsSync,
+  unlinkSync: fs.unlinkSync,
+  mkdirSync: fs.mkdirSync,
+  readdirSync: fs.readdirSync,
+  openSync: fs.openSync,
+  readSync: fs.readSync,
+  closeSync: fs.closeSync,
+  rmSync: _,
+  // Asynchronous methods
+  appendFile: fsPromises.appendFile,
+  readFile: fsPromises.readFile,
+  access: fsPromises.access,
+  stat: fsPromises.stat,
+  open: fsPromises.open,
+};
+
+function _(dirPath: string) {
+  const files = customFileSystem.readdirSync(dirPath);
+
+  for (const file of files) {
+    const filePath = path.join(dirPath, file);
+    customFileSystem.unlinkSync(filePath);
+  }
+  fs.rmSync(dirPath, { force: true, recursive: true });
+}
+
+export type FileSystemI = typeof customFileSystem;
