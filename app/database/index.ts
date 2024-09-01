74da588c4e52d676bdb8f66177b4c16cf1318ba7
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-01 13:32:48 +0300

    refactor: introduce lazy initialization for Database instance
    - Added a singleton pattern for the Database instance to ensure only one instance is created and reused.
    - Injected the custom filesystem

--- app/database/index.ts
+++ app/database/index.ts
@@ -1,6 +1,16 @@
 import * as path from "path";
-
 import { Database } from "./Database.js";
+import { customFileSystem as fs } from "../lib/fileSystem.js";
+let instanse: Database;
 
-const dbPath = path.join(process.cwd(), "database");
-export const db = new Database("my-db", dbPath);
+function init() {
+  if (instanse) {
+    return instanse;
+  }
+  const dbPath = path.join(process.cwd(), "database");
+  instanse = new Database("my-db", dbPath, fs);
+
+  return instanse;
+}
+
+export const db = init();
