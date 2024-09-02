cde56e936ef6b6de433a376d8a9ae8854a1626a4
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-02 10:13:24 +0300

    test(Table): add tests for row operations and table creation

--- tests/database/Table.test.ts
+++ tests/database/Table.test.ts
@@ -16,10 +16,10 @@ import Table from "../../app/database/_Table.js";
 const DB_NAME = "TEST";
 const dbPath = path.join(process.cwd(), DB_NAME);
 const mockTodos = [
-  { description: "todo description - 1", id: "1", title: "Todo 1" },
-  { description: "todo description - 2", id: "2", title: "Todo 2" },
-  { description: "todo description - 3", id: "3", title: "Todo 3" },
-  { description: "todo description - 4", id: "4", title: "Todo 4" },
+  { description: "todo description - 1", title: "Todo 1" },
+  { description: "todo description - 2", title: "Todo 2" },
+  { description: "todo description - 3", title: "Todo 3" },
+  { description: "todo description - 4", title: "Todo 4" },
 ];
 
 function clean() {
@@ -32,24 +32,19 @@ function clean() {
 
 function setupDb() {
   const db = new Database(DB_NAME, dbPath, fs);
-  const tableTodo = db.createOrGetTable("todo", ["id", "title", "description"]);
-  const tableUsers = db.createOrGetTable("users", [
-    "id",
-    "firstName",
-    "lastName",
-  ]);
+  const tableTodo = db.createOrGetTable("todo", ["title", "description"]);
+  const tableUsers = db.createOrGetTable("users", ["firstName", "lastName"]);
   return { tableTodo, tableUsers };
 }
 
 async function loadTableWithTodos(
-  table: Table<["id", "title", "description"]>,
+  table: Table<["title", "description"]>,
   rows = 50
 ) {
   for (let i = 10; i < rows + 10; i++) {
     await table.createRow({
-      id: i.toString(),
-      description: `Description - ${i}`,
       title: `Title - ${i}`,
+      description: `Description - ${i}`,
     });
   }
 }
@@ -89,10 +84,10 @@ describe("Table class tests", () => {
     await tableTodo.createRow(todo);
 
     const exist = await tableTodo.fetchOneRow({
-      where: { id: todo.id },
+      where: { id: "1" },
     });
 
-    expect(exist).toEqual(todo);
+    expect(exist?.title).toEqual(todo.title);
   });
   // Unit
   test("should increase table size after inserting a new row", async () => {
@@ -105,6 +100,15 @@ describe("Table class tests", () => {
 
     const newSize = tableTodo.tableSize;
     expect(newSize).toBeGreaterThan(oldSize);
+  });
+
+  test("should increase table internal id after inserting a new row", async () => {
+    const { tableTodo } = setupDb();
+
+    await loadTableWithTodos(tableTodo, 20);
+
+    const internalIdCount = (tableTodo as any)._idCounter;
+    expect(internalIdCount).toEqual(20);
   });
 
   test("should fetch the correct number of rows", async () => {
@@ -139,7 +143,7 @@ describe("Table class tests", () => {
     const { tableTodo } = setupDb();
 
     await loadTableWithTodos(tableTodo);
-    const rowToFind = { id: "undefiend" };
+    const rowToFind = { title: "undefiend" };
     try {
       await tableTodo.deleteRow({ where: rowToFind });
     } catch (error: any) {
