7ba82e5a227a182249c0ba19bff756229a1a4e08
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-08 14:52:33 +0300

    feat: Add editTodoHandler and update response status for createTodo
    Implemented editTodoHandler to handle updating existing todos by ID
    Added validation for todoId in editTodoHandler with appropriate error handling

--- app/handlers/rest/todoHandler.ts
+++ app/handlers/rest/todoHandler.ts
@@ -2,15 +2,18 @@ import type { Request, Response } from "../../types.js";
 import {
   createTodoService,
   getTodosService,
+  editTodoService,
 } from "../../services/todoService.js";
 
 export async function getTodosHandler(req: Request, res: Response) {
-  const todo = await getTodosService();
+  const todos = await getTodosService();
 
   return res
     .status(200)
-    .set({ "Content-Type": "application/json" })
-    .send(JSON.stringify(todo));
+    .set({
+      "Content-Type": "application/json",
+    })
+    .send(JSON.stringify(todos));
 }
 
 export async function createTodoHandler(req: Request, res: Response) {
@@ -20,7 +23,26 @@ export async function createTodoHandler(req: Request, res: Response) {
     await createTodoService(data);
 
     res
-      .status(204)
+      .status(200)
+      .set("Content-type", "application/json")
+      .end(JSON.stringify(data));
+  } catch (error: any) {
+    res.status(400).end(error.message);
+  }
+}
+
+export async function editTodoHandler(req: Request, res: Response) {
+  try {
+    const todoId = req.params.id;
+    const data = validateCreateTodoBody(req.body);
+
+    if (!todoId) {
+      return res.status(403).end(JSON.stringify({ error: "Invalid todo id" }));
+    }
+    await editTodoService(todoId, data);
+
+    res
+      .status(200)
       .set("Content-type", "application/json")
       .end(JSON.stringify(data));
   } catch (error: any) {
