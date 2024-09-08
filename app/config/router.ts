a991d20b9649e899e7268a499c253ae8748f502e
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-08 14:55:19 +0300

    feat: Add CORS middleware to router initialization

--- app/config/router.ts
+++ app/config/router.ts
@@ -2,11 +2,14 @@ import Router from "../core/routing/index.js";
 import bodyParser from "../core/lib/bodyParser.js";
 import initRoutes from "../routes/initRoutes.js";
 
+import cors from "./cors.js";
+
 function initRouter() {
   const router = Router.getInstance();
 
   router.use(bodyParser.json());
   router.use(bodyParser.urlencoded());
+  router.use(cors());
 
   initRoutes(router);
 
