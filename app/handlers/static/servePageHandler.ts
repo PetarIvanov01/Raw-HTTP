54d5318d709053fe54c060e4f0effe6837feef63
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-08 14:57:47 +0300

    feat: Add cache validation to homePageHandler with Last-Modified header
    - check Last-Modified timestamp for index file
    - add cache validation using the If-Modified-Since header to send 304 status when content is unchanged
    - Updated response headers to include Cache-Control and Last-Modified for better caching support

--- app/handlers/static/servePageHandler.ts
+++ app/handlers/static/servePageHandler.ts
@@ -1,22 +1,44 @@
 import type { Request, Response } from "../../types.js";
-import { readFileFromPublic } from "../../helpers/fileHelpers.js";
+import { getFileStat, readFileFromPublic } from "../../helpers/fileHelpers.js";
 
 import handleNotFound from "./notFoundHandler.js";
 
 export default async function homePageHandler(req: Request, res: Response) {
   try {
-    const file = await readFileFromPublic("index.html.gz", "gz");
+    const indexFilePath = "index.html.gz";
+    const file = await readFileFromPublic(indexFilePath, "gz");
 
     if (!file) {
       return handleNotFound(res);
     }
 
+    const stats = await getFileStat(indexFilePath);
+
+    if (!stats) {
+      return handleNotFound(res);
+    }
+
+    const lastModified = stats.mtime.toUTCString();
+    const ifModifiedSince = req.headers["if-modified-since"];
+
+    if (ifModifiedSince) {
+      const clientModifiedDate = new Date(ifModifiedSince);
+
+      const serverModifiedDate = new Date(stats.mtime);
+      serverModifiedDate.setMilliseconds(0);
+
+      if (clientModifiedDate >= serverModifiedDate) {
+        return res.status(304).end("");
+      }
+    }
     res
       .status(200)
       .set({
         "Content-Type": "text/html",
         "Content-Length": file.byteLength,
         "Content-Encoding": "gzip",
+        "Cache-Control": "no-cache, must-revalidate",
+        "Last-Modified": lastModified,
       })
       .send(file);
   } catch (error) {
