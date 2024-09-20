4e5e5c1b0540535dc1922101bec776741fc7545e
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-20 22:06:01 +0300

    feat: Add keep-alive and timeout handling - set keepAlive option with a KEEP_ALIVE_DELAY of 20 seconds before sending the first keep-alive probe to test the network connection - add a MAX_TIMEOUT of 60 seconds to close inactive connections

--- app/config/server.ts
+++ app/config/server.ts
@@ -2,28 +2,42 @@ import * as net from "net";
 import { Router } from "../types.js";
 import { RequestParser } from "../core/routing/index.js";
 
+const KEEP_ALIVE_DELAY = 20000;
+const MAX_TIMEOUT = 60000;
 export default function createServer(router: Router) {
-  return net.createServer((socket) => {
-    const clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
-    // console.log(`[INFO] New connection established from ${clientAddress}`);
+  return net.createServer(
+    { keepAlive: true, keepAliveInitialDelay: KEEP_ALIVE_DELAY },
+    (socket) => {
+      const clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
+      // console.log(`[INFO] New connection established from ${clientAddress}`);
 
-    socket.on("data", (data) => {
-      // console.log(`[INFO] Received data from ${clientAddress}:`);
-      const parser = new RequestParser(data);
-      router.dispatchIncomingRequest(parser, socket);
-      // console.log(`[INFO] Dispatched request from ${clientAddress} to router`);
-    });
+      socket.setTimeout(MAX_TIMEOUT);
 
-    socket.on("close", () => {
-      // console.log(`[INFO] Connection closed with ${clientAddress}`);
-      socket.end();
-    });
+      socket.on("timeout", () => {
+        console.log(`[INFO] Connection timed out with ${clientAddress}`);
+        socket.end();
+      });
 
-    socket.on("error", (err) => {
-      console.error(`[ERROR] An error occurred with ${clientAddress}:`, err);
-      socket.end(
-        `HTTP/1.1 500 Internal Server Error\r\n\ ${JSON.stringify(err)}\r\n`
-      );
-    });
-  });
+      socket.on("data", (data) => {
+        // console.log(`[INFO] Received data from ${clientAddress}:`);
+        const parser = new RequestParser(data);
+        router.dispatchIncomingRequest(parser, socket);
+        console.log(
+          `[INFO] Dispatched request from ${clientAddress} to router`
+        );
+      });
+
+      socket.on("close", () => {
+        console.log(`[INFO] Connection closed with ${clientAddress}`);
+        socket.end();
+      });
+
+      socket.on("error", (err) => {
+        console.error(`[ERROR] An error occurred with ${clientAddress}:`, err);
+        socket.end(
+          `HTTP/1.1 500 Internal Server Error\r\n\ ${JSON.stringify(err)}\r\n`
+        );
+      });
+    }
+  );
 }
