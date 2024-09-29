49270c5887b704f2d262224cd3dac24d2eff9003
PetarIvanov01 <123456789petar45@gmail.com>
2024-09-29 20:54:34 +0300

    feat: Create edit todo function that calls server handler

--- public/src/service/todoService.js
+++ public/src/service/todoService.js
@@ -1,35 +1,45 @@
-const BASE_URL = "http://localhost:4221"
+const BASE_URL = "http://localhost:4221";
 
 const endpoints = {
-    "getAll": '/todo',
-    "create": "/todo"
-}
+  getAll: "/todo",
+  create: "/todo",
+  edit: (id) => `/todo/${id}`,
+};
 
 async function request(url, method = "GET", data = null, headers = {}) {
+  const request = await fetch(url, {
+    method,
+    headers,
+    body: data,
+  });
 
-    const request = await fetch(url, {
-        method,
-        headers,
-        body: data
-    })
+  if (!request.ok) {
+    throw new Error("Failed request.");
+  }
 
-    if (!request.ok) {
-        throw new Error("Failed request.")
-    }
-
-    if (request.status === 204) {
-        return;
-    }
-    return request.json();
+  if (request.status === 204) {
+    return;
+  }
+  return request.json();
 }
 
 export async function getTodos() {
-    const url = BASE_URL + endpoints['getAll']
-    return await request(url)
+  const url = BASE_URL + endpoints.getAll;
+  return await request(url);
 }
 
 export async function createTodo(todo) {
-    const url = BASE_URL + endpoints['getAll']
+  const url = BASE_URL + endpoints.create;
 
-    return await request(url, "POST", JSON.stringify(todo), { "Content-Type": "application/json" })
+  return await request(url, "POST", JSON.stringify(todo), {
+    "Content-Type": "application/json",
+  });
+}
+
+export async function editTodo(id, todo) {
+  const url = BASE_URL + endpoints.edit(id);
+
+  return await request(url, "PUT", JSON.stringify(todo), {
+    "Content-Type": "application/json",
+  });
 }
